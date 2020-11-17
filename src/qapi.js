18833473076727132fcd29075e3d6977ca00caa2
c0jkute <c0jkute20@gmail.com>
2020-11-17 13:45:42 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb3704cdc290b001ef20e67",
+				"5fb3704cdc290b001ef20e69"
+			]
+		},
+		"5fb3704cdc290b001ef20e67": {
+			"id": "5fb3704cdc290b001ef20e67",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb3704cdc290b001ef20e69": {
+			"id": "5fb3704cdc290b001ef20e69",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
